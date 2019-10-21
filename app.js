d4466bb07e61ad8ab759f33149b2c8d41adb8749
JoseCabreraJC <josem.cabrera259@gmail.com>
2019-10-21 10:49:37 -0300

    added package.json and CRUD operations

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-const http = require('http');
-// const express = require('express');
+const express = require('express');
+const app = express();
 
-// const app = express();
-const hostname = '127.0.0.1';
+app.use(express.json());
+
+const productos = [
+    {id: 1,
+        nombre: 'Yogurt'},
+    {id: 2,
+        nombre: 'Yerba'},
+    {id: 3,
+        nombre: 'Cafe'}
+    ]
 const port = 3000;
 
-// app.get(hostname,(req,res) => {
-//     res.send('Hello World');
-// })
+app.get('/',(req,res) => {
+    res.send('Inicio de esta cosa');
+})
 
-// app.listen(port)
-
-
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hola Mundo');
+app.get('/api/productos/',(req,res) => {
+    res.send(productos);
 });
 
-server.listen(port, hostname,()=>{
-    console.log(`El servidor esta corriendo en http://${hostname}:${port}/`);
+app.get('/api/productos/:id',(req,res) => {
+    const resultado = productos.find(producto => producto.id == parseInt(req.params.id));
+    if (!resultado) return res.status(404).send('No es encuentra ese producto')
+    res.send(resultado);
+});
+
+app.post('/api/productos/',(req,res)=>{
+    let nombreProducto =req.body.nombre; 
+    const producto = {id : productos.length+1, nombre:nombreProducto};
+    productos.push(producto);
+    res.send(producto);
+})
+
+app.put('/api/productos/:id',(req,res)=>{
+    const producto = productos.find(producto => producto.id == parseInt(req.params.id));
+    producto.nombre= req.body.nombre;
+    res.send(producto);    
+})
+
+app.delete('/api/productos/:id',(req,res)=>{
+    const producto = productos.find(producto => producto.id == parseInt(req.params.id));
+    productos.splice(producto,1);
+    res.send(producto);
+})
+
+app.listen(port,() => {
+    console.log(`El servidor esta corriendo en http://localhost:${port}/`)
 });
